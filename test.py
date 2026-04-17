54578532f1fff63d4c993d6be8eb065af1f873c1
kirshtalreja24 <161924216+kirshtalreja24@users.noreply.github.com>
2026-04-17 14:30:01 +0500

    .

--- test.py
+++ test.py
@@ -1,19 +1,52 @@
 from DocumentExtraction import Extractedfiles
 from procecssor import InvertedIndex
-
-files = Extractedfiles()
-files.readData()
-files = files.getfiles()
-print(len(files))
-print(files[0])
+from Queries import Queries
 
 
-obj = InvertedIndex()
-obj.documentProcessing()
-# obj.writeToFile()
+def main():
 
 
+    files = Extractedfiles()
+    files.readData()
+    docs = files.getfiles()
 
-#get posting list for a specific word
-arr = obj.getSpecificPostingList("hammer")
-print(arr)
+    print("Total Documents:", len(docs))
+    print("\nSample Document:\n")
+    # print(docs[0][:500]) 
+
+    print("\nBuilding Inverted Index...\n")
+
+    processor = InvertedIndex()
+    processor.documentProcessing()
+
+    print("Index built successfully!")
+    print("Vocabulary size:", len(processor.words))
+
+    # processor.writeToFile()
+
+
+    query_engine = Queries(processor)
+
+    test_queries = [
+        " massive inflow of refugees",
+    ]
+
+    print("\n==============================")
+    print("TESTING VSM QUERY SYSTEM")
+    print("==============================\n")
+
+    for q in test_queries:
+        print(f"\n🔍 Query: {q}")
+
+        results = query_engine.process_query(q)
+
+        if not results:
+            print("❌ No results found")
+        else:
+            print("📄 Top Results:")
+            for doc_id, score in results[:5]:
+                print(f"Doc {doc_id} | Score: {round(score, 4)}")
+
+
+if __name__ == "__main__":
+    main()
